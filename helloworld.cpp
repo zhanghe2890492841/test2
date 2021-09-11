225ffd966719a1e7159c72b5dda20695b40dcf65
zhanghe2890492841 <18635532911@163.com>
2021-09-11 19:25:48 +0800

    master

--- helloworld.cpp
+++ helloworld.cpp
@@ -5,7 +5,7 @@ int main(int argc,char **argv)
     int val1 =10;
     int val2 =20;
     std::cout << "before swap:" << std::endl;
-    std::cout << "val1 =" << val1 <<std:: endl;
+    std::cout << "val1 =" << val1 <<std:: endl;// haha
     std::cout << "val2=" << val2 <<std:: endl;
     swap(val1,val2);
     std::cout << "after swap:" << std::endl;
